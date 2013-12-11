9b5ca175953dd7345681cae94a39550e38301cc7
Peter Jones <kephail@me.com>
2013-12-11 13:25:06 +0000

    added css, styled index, edited layout etc

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 mysql_connect("localhost", "root", "root");
 mysql_select_db("twilioNotes");
+
+$currentDate = new DateTime(date('y-m-d h:i:s'));
+$currentDate = $currentDate->format('y-m-d');
+
 ?>
 
 
@@ -9,28 +13,41 @@ mysql_select_db("twilioNotes");
 <head>
 	<meta charset="UTF-8">
 	<title>Notes - Twillio</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-	<div class='header'>
-		Notes
+	<div class="wrapper">
+		<header>
+			<h1>Twilio - TODO App</h1>
+		</header>
+		<div class="lines"></div>
+		<ul class="list">
+			<li><h2>To-Do:</h2></li>
+			<?php
+			$sql = mysql_query("SELECT * FROM notes ORDER BY NoteTime");
+			while ($row = mysql_fetch_array($sql)){
+				$id = $row ['NoteID'];
+				$desc = $row ['NoteDesc'];
+				$datetime = new DateTime($row ['NoteTime']);
+				$date = $datetime->format('y-m-d');
+				$time = $datetime->format('h:i');
+			?>
+			<li>
+				<?php 
+				if ($date == $currentDate) {
+					echo "<strong>" .  $time . "</strong>";
+				}else{
+					echo "<strong>" .  $date . "</strong>";
+				}
+				?>
+				<?php echo " - " ?>
+				<?php echo $desc ?>
+			</li>
+		<?php
+		};
+		?>
+		</ul>
 	</div>
-
-	<?php
-	$sql = mysql_query("SELECT * FROM notes ORDER BY NoteID DESC");
-	while ($row = mysql_fetch_array($sql)){
-		$id = $row ['NoteID'];
-		$desc = $row ['NoteDesc'];
-		$date = $row ['NoteTime'];
-	?>
-	<div class='notes_main'>
-		<?php echo $desc ?>
-		<?php echo $date ?>
-	</div>
-
-	<?php
-	};
-	?>
-
 </body>
 </html>
 
