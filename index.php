2375b6547957614855df254cf8b608daf9ba798e
Rob Calcroft <rob@calcroft.com>
2013-12-11 19:16:12 +0000

    Added Fork me on GitHub ribbon to site

--- index.php
+++ index.php
@@ -16,6 +16,12 @@ $currentDate = $currentDate->format('d-m-y');
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+	<!-- GitHub Ribbon -->
+	<div>
+		<a href="https://github.com/kephail/Twilionotes"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png" alt="Fork me on GitHub"></a>
+	</div>
+	<!-- End Ribbon -->
+
 	<div class="wrapper">
 		<header>
 			<h1>Twilio - TODO App</h1>
