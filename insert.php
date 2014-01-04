a865b76a5a2723095ecc3ae635a12bfa4e63910d
Rob Calcroft <rob@calcroft.com>
2014-01-04 00:08:15 +0000

    added git ignore and updated insert.php

--- insert.php
+++ insert.php
@@ -3,14 +3,17 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
     mysql_connect("cust-mysql-123-18", "twilio", "twilio123");
-	mysql_select_db("twilio");
+    mysql_select_db("twilio");
 
     $description = $_REQUEST['Body'];
 
     if($description == 'Helpme') { 
-    	$help = "To-Do Description, DD-MM-YY HH:MM";
-    	?><Response><SMS><?php echo $help; ?></SMS></Response><?php
-    } else {
+        $help = "To-Do Description, DD-MM-YY HH:MM";
+        ?>
+            <Response><SMS><?php echo $help; ?></SMS></Response>
+        <?php
+        exit;
+    };
 
     $new = str_split($description);
 
@@ -18,26 +21,25 @@
     $date = '';
 
     foreach($new as &$value) {
-    	if($value == ',') {
-    		$bool = true;
-    	}
-    	if($bool == false){
-    		$descrip .= $value;
-    	}
-    	if ($bool == true) {
-    		$date .= $value;
-    	}
-    }
+        if($value == ',') {
+            $bool = true;
+        }
+        if($bool == false){
+            $descrip .= $value;
+        }
+        if ($bool == true) {
+            $date .= $value;
+        }
+    };
     
     if($date[0] == ',' && $date[1] == ' ') {
-    	$date = ltrim($date, ", ");
-	} elseif($date[0] == ',') {
-		$date = ltrim($date, ",");
-	};
-	$date .= ":00";
-	mysql_query("INSERT INTO notes (NoteDesc, NoteTime) VALUES ('$descrip', '$date')");
-	?>
-		<Response>
-		  <SMS><?php echo "You've added $descrip to your to-do-list. You will be reminded at $date. Visit http://www.everythingrob.co.uk/Twilionotes. Call 01143031702 to hear your to-dos"; ?></SMS>
-	    </Response>
-	<?php }; ?>
+        $date = ltrim($date, ", ");
+    } elseif($date[0] == ',') {
+        $date = ltrim($date, ",");
+    };
+    $date .= ":00";
+    mysql_query("INSERT INTO notes (NoteDesc, NoteTime) VALUES ('$descrip', '$date')");
+    ?>
+        <Response>
+          <SMS><?php echo "You've added $descrip to your to-do-list. You will be reminded at $date. Visit http://www.everythingrob.co.uk/Twilionotes. Call 01143031702 to hear your to-dos"; ?></SMS>
+        </Response>
